f4c0d00ce08cf80e4ce46cf7f456ab210ba6fe6e
Brandon DeRosier <x@bdero.me>
2015-12-17 21:18:41 -0500

    Added proper logo resizing

--- scripts/menu_logo_scale.gd
+++ scripts/menu_logo_scale.gd
@@ -1,8 +1,13 @@
-
 extends Node2D
 
+
+var default_size
+const VEC2_IDENT = Vector2(1, 1)
+
 func _ready():
+	default_size = Vector2(Globals.get("display/width"), Globals.get("display/height"))
 	set_process(true)
 
 func _process(delta):
-	var vp = get_viewport_rect().size
+	var vp_width = get_viewport_rect().size.width
+	self.set_scale(VEC2_IDENT*vp_width/default_size.width)
