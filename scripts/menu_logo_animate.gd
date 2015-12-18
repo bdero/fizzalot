74ee2909b714e6270d0969bf689a84c8216628d7
Brandon DeRosier <x@bdero.me>
2015-12-17 22:07:55 -0500

    Added bobbing animation

---
+++ scripts/menu_logo_animate.gd
@@ -0,0 +1,14 @@
+extends Sprite
+
+
+var default_pos
+var time = 0
+
+func _ready():
+	default_pos = get_pos()
+	set_process(true)
+
+func _process(delta):
+	time += delta
+	set_pos(default_pos + Vector2(0, sin(time*2.4)*10))
+	set_rot(sin(time*1.15)/10)
