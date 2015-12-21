80ffba063ece38c802edc3c169808f0e8e8544e3
Brandon DeRosier <x@bdero.me>
2015-12-20 23:34:59 -0500

    Soda bottle animations

--- assets/models/Bottle.gd
+++ assets/models/Bottle.gd
@@ -1,8 +1,17 @@
-
 extends Spatial
 
+
+var time = 0
+var animations
+
 func _ready():
+	animations = get_node("animations")
+	animations.play("default")
 	set_process(true)
 
 func _process(delta):
+	time += delta
 	rotate_y(0.5*delta)
+
+	if time > 2.5 && animations.get_current_animation() != "pop":
+		animations.play("pop")
